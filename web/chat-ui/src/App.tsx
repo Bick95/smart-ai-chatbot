8e272e3a6bc7ba5c0d5b9c2330fda6b566e86e38
Daniel Bick <daniel.bick@live.de>
2026-02-18 04:01:31 +0100

    Getting started adding Tailwind/Shadcn

--- web/chat-ui/src/App.tsx
+++ web/chat-ui/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import { Button } from "@/components/ui/button"
 
 
 export default function App() {
     return (
         <>
             <h1>Smart AI Chatbot</h1>
+            <Button>Click me</Button>
         </>
     )
 }
